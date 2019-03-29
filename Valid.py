7420b6363e16ddefbb55accb927acdb7266e331f
Deepanshu Bansal <dbansal@cse.iitk.ac.in>
2019-03-29 22:58:11 +0530

    Update Valid.py to handle only probability values in matrix

--- Valid.py
+++ Valid.py
@@ -6,6 +6,10 @@ def isValidTransitionMatrix(matrix):
 		print("Transition matrix should be a square matrix")
 		return False
 	
+	if not((matrix >= 0).all() and (matrix <= 1).all()):
+		print("All elements should be between [0, 1]")
+		return False
+
 	rowSum = matrix.sum(axis=1)
 
 	for idx in range(rowSum.shape[0]):
