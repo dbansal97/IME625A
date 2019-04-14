fd824a728bd5f47c90c256513fbf16be3bb7e3bc
Deepanshu Bansal <dbansal@cse.iitk.ac.in>
2019-04-14 21:21:08 +0530

    Add ExpectedNumVisits before absorption

---
+++ ExpectedNumVisits.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def expectedNumVisits(Q_matrix):
+	Q_matrix = np.array([[0,.5,0], [.5 ,0 ,.5], [0 ,.5 ,0]])
+
+	I = np.identity(Q_matrix.shape[0], dtype = float)
+	N = np.linalg.inv(I-Q_matrix)
+
+	state = int(input("Enter the starting state (0 indexed): "))
+	if state >= Q_matrix.shape[0] or state < 0:
+		print("You should've entered a valid state. Bye!!")
+		return -1
+	
+	print("Starting from state ", state, " expected number of visits in respective states before absorption is :")
+	for idx in range(N.shape[0]):
+		print("State ", idx, " expected visits = ", N[state][idx])
+	
+	rowSum = N.sum(axis=1)
+	print("Time till absorption is ", rowSum[state], " given we start in state ", state)	
+	return 1
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from ReadMatrix import readTransitionMatrix
 from nthStepProb import nthStepStates
+from ExpectedNumVisits import expectedNumVisits
 
 matrix = readTransitionMatrix()
 print(matrix)
 
 nthStepStates(matrix)
+expectedNumVisits(matrix)
