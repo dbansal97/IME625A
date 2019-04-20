58107996b40a09724662f91a0377d5d30664591e
Anupriy <anupriy97@gmail.com>
2019-04-20 11:39:34 +0530

    Pagerank Modified

--- PageRank.py
+++ PageRank.py
@@ -15,26 +15,10 @@ def printPages(stationaryDist, stateNames, numPages = 10):
 	indicies = stationaryDist.argsort()[-numPages:][::-1]
 
 	for idx in range(indicies.shape[0]):
-		print("Page at Rank ", idx+1, " is ", stateNames[indicies[idx]],\
-		 " with probability ", round(Decimal(stationaryDist[indicies[idx]]), 7))
+		print("Page at Rank ", idx+1, " is ", stateNames[indicies[idx]])
+		# print("Page at Rank ", idx+1, " is ", stateNames[indicies[idx]],\
+		#  " with probability ", round(Decimal(stationaryDist[indicies[idx]]), 4))
 
-def pagerankWiki(M, startDist, eps=1.0e-4, d=0.85):
-    N = M.shape[1]
-    v = np.random.rand(N, 1)
-    # v = startDist
-    v = v / np.linalg.norm(v, 1)
-    last_v = np.ones((N, 1), dtype=np.float32) * 100
-
-def pagerankWiki(M, eps=1.0e-8, d=0.85):
-    N = M.shape[1]
-    v = np.random.rand(N, 1)
-    v = v / np.linalg.norm(v, 1)
-    last_v = np.ones((N, 1), dtype=np.float32) * 100
-    
-    while np.linalg.norm(v - last_v, 2) > eps:
-        last_v = v
-        v = d * np.matmul(M, v) + (1 - d) / N
-    return v
 
 def ourPageRank(stateNames, matrix, startDist):
 	cb, residual = findCB(matrix)
@@ -90,54 +74,29 @@ def Loaded(matrix, startDist):
 	# print(states)
 
 	# expectedNumVisits(Q, states)
-	hitProb = hittingProbs(Q, R, states)
+	hitProb = hittingProbs(Q, R, states, 1)
 
-	np.save("hitProb", hitProb)
-	
 	stationaryDist = findStationaryDistOverAll(matrix, startDist, states, hitProb)
 	# print("Stationary Distribution :")
 	# print(stationaryDist)
 	return stationaryDist
 
+
 def main():
 	stateNames, matrix = loadPageRankData()
+	
 	startDist = np.zeros(matrix.shape[0])
 	startDist.fill(1/matrix.shape[0])
-
-	print("Going for Loaded")
-	ourStationary = Loaded(matrix, startDist)
-	print("Going for Wiki")
-	wikiStationary = pagerankWiki(matrix.T, startDist)
-	
-	print("For Wiki")
-	printPages(wikiStationary, stateNames)
-	print(wikiStationary)
-
-
-	print("For Us")
-	printPages(ourStationary, stateNames)
-	print(ourStationary)
-
-def printPages(stationaryDist, stateNames, numPages = 10):
-	stationaryDist = stationaryDist.reshape(stationaryDist.shape[0])
-	indicies = stationaryDist.argsort()[-numPages:][::-1]
-
-	for idx in range(indicies.shape[0]):
-		print("Page at Rank ", idx+1, " is ", stateNames[indicies[idx]],\
-		 " with probability ", round(Decimal(stationaryDist[indicies[idx]]), 7))
-
-
-def main():
-	stateNames, matrix = loadPageRankData()
-
-	v = pagerankWiki(matrix.T)
-	print(v)
-	
+	v = Loaded(matrix, startDist)
+	print("When each starting state has equal probability")
 	printPages(v, stateNames)
-	# startDist = np.zeros(matrix.shape[0])
-	# startDist.fill(1/matrix.shape[0])
-	# print(startDist)
-	# ourPageRank(stateNames, matrix, startDist)
+
+	startDist = np.random.rand(startDist.shape[0])
+	startDist = startDist/np.sum(startDist)
+	v = Loaded(matrix, startDist)
+	print("When each starting state has random probability")
+	printPages(v, stateNames)
+
 
 if __name__ == "__main__":
 	main()
